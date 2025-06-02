c389f3150b6955b1ee25c6cf1fb10d866cb3c465
Jhojan J. Sanchez Q. <jhojansanchez27@gmail.com>
2025-06-02 10:18:54 -0400

    lab12

--- main.js
+++ main.js
@@ -92,7 +92,6 @@ app.delete('/users/:id', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
